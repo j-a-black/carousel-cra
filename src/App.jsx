0a1f53f1c46eef278359000217ecf5f16c8a0359
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2022-02-23 20:00:04 -0600

    added navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+
+import "./app.scss";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
